f97c3f28562a8ddff5b4a57ce7f624e9279ef67e
Ahmad-Zainur-Royyan <ahmad_royyan_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:12:58 +0700

    Add files via upload

---
+++ Abc.java
@@ -0,0 +1,6 @@
+public class Abc {
+    public static void main(String[] args) {
+        String nama = "Royyan";
+        String kelas = "X RPL 5";
+        System.out.print("Halo, nama saya Royyan" + nama +" dari kelas " +kelas);
+}
